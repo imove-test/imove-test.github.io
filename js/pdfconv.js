f9fd318a9775a9d022cd7f6a722e0fdd8fd34254
Sharif Mahfouz <sharif.mahfouz@gmail.com>
2014-09-17 12:23:23 -0400

    Actually updated pdfconv.js

--- js/pdfconv.js
+++ js/pdfconv.js
@@ -1,6 +1,17 @@
 var doc = new jsPDF();
+var specialElementHandlers = {
+	'#ignorePDF': function(element,renderer) {
+		return true;
+	}
+};
 
 // Render section with id="render"
-doc.fromHTML($('#render').get(0), 15, 15 {
-	'width': 170
-});
+doc.fromHTML($('#render').get(0),
+	15,		// x coord
+	15,		// y coord
+	{
+		'width': 170, // max width of contents
+		'elementHandlers': specialElementHandlers
+	});
+
+doc.output('dataurlnewwindow');
