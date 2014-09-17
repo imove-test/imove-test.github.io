5fe0f1a8e47e290a47797821dd981a696e6bca15
Sharif Mahfouz <sharif.mahfouz@gmail.com>
2014-09-17 13:39:39 -0400

    Added code inside function pdfconv().

--- js/pdfconv.js
+++ js/pdfconv.js
@@ -1,17 +1,19 @@
-var doc = new jsPDF();
-var specialElementHandlers = {
-	'#ignorePDF': function(element,renderer) {
-		return true;
-	}
-};
-
-// Render section with id="render"
-doc.fromHTML($('#render').get(0),
-	15,		// x coord
-	15,		// y coord
-	{
-		'width': 170, // max width of contents
-		'elementHandlers': specialElementHandlers
-	});
-
-doc.output('dataurlnewwindow');
+function pdfconv() {
+	var doc = new jsPDF();
+	var specialElementHandlers = {
+		'#ignorePDF': function(element,renderer) {
+			return true;
+		}
+	};
+	
+	// Render section with id="render"
+	doc.fromHTML($('#render').get(0),
+		15,		// x coord
+		15,		// y coord
+		{
+			'width': 170, // max width of contents
+			'elementHandlers': specialElementHandlers
+		});
+	
+	doc.output('dataurlnewwindow');
+}
