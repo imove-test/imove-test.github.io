292ff90147a5b390567f9d248a722f25178a12e2
Sharif Mahfouz <sharif.mahfouz@gmail.com>
2014-10-19 22:27:20 -0400

    Tested creategraph.js. Creates basic line graph, given a DOM element and one array of data (i.e., Tardieu numbers). Need to add axes, date handling (i.e., scale the graph appropriately), etc.

--- js/create-graph.js
+++ js/create-graph.js
@@ -1,5 +1,18 @@
-function creategraph() {
-	// At 10, 50, canvas of size 640 x 480
-	var r = Raphael(10,50,640,400);
-	r.linechart(10,50,640,400,[1,2,3],[1,2,3]);
+// Given an input of the DOM element and data, create a graph.
+function creategraph(element,data) {
+	// Canvas of size 640 x 480 in DOM element "element"
+	var r = Raphael(element,640,480),
+		txtattr = {font:"20px sans-serif"};
+	r.text(340,10,"Results").attr(txtattr);
+	r.linechart(0,20,640,460,range(1,data.length),data);
+	// console.log("Graph created");
+}
+
+// Used to generate x values (so that the points are spread equally.
+function range(start,end) {
+    var array = [];
+    for (var i = start; i <= end; i++) {
+        array.push(i);
+    }
+    return array;
 }
