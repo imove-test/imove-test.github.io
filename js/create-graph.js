befb6f0b630222e89b8055dcb40dc395a89f07f7
Sharif Mahfouz <sharif.mahfouz@gmail.com>
2014-10-22 19:29:39 -0400

    Should work now. Give it input of dates and should view accordingly.

--- js/create-graph.js
+++ js/create-graph.js
@@ -1,19 +1,38 @@
-// Given an input of the DOM element and data, create a graph.
+// Given an input of the DOM element, data, and dates in a string
+// form that can be converted to a Date object, create a graph.
 function creategraph(element,data,dates) {
 	// Canvas of size 640 x 480 in DOM element "element"
+	console.log(datesToInts(dates));
 	var r = Raphael(element,640,480),
 		txtattr = {font:"20px sans-serif"};
 	r.text(340,10,"Tardieu Scale Results").attr(txtattr);
-	r.linechart(10,20,630,460,range(1,data.length),data,{axis: "0 0 0 1", symbol: 'circle'}).hoverColumn(function () {
+	r.linechart(10,20,630,460,datesToInts(dates),data,{axis: "0 0 0 1", symbol: 'circle'}).hoverColumn(function () {
 		this.tags = r.set();
-		console.log(this.tags);
 		for (var i = 0, ii = this.y.length; i < ii; i++) {
-			this.tags.push(r.tag(this.x, this.y[i], dates[i], 160, 10).insertBefore(this).attr([{ fill: "#fff" }, { fill: this.attr("fill") }]));
+			this.tags.push(r.tag(this.x, this.y[i], intsToDates(this.values)[i], 160, 10).insertBefore(this).attr([{ fill: "#fff" }, { fill: this.attr("fill") }]));
 			}
 		}, function () {
 			this.tags && this.tags.remove();
 	});
 	// console.log("Graph created");
+}
+
+// Convert array of dates (in string format) to ints.
+function datesToInts(dates) {
+	var ints = [];
+	for (var i = 0; i < dates.length; i++) {
+		ints.push(new Date(dates[i]).getTime());
+	}
+	return ints;
+}
+
+// Convert array of dates (in int format) to string.
+function intsToDates(ints) {
+	var dates = [];
+	for (var i = 0; i < ints.length; i++) {
+		dates.push(new Date(ints[i]));
+	}
+	return dates;
 }
 
 // Used to generate x values (so that the points are spread equally.
